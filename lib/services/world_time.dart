79888d0e679a2753041317e163d7ce82b69e988b
Vaibhav Muchandi <vaibhavmm@hotmail.com>
2020-07-08 04:28:51 +0530

    Single place time added with UI

---
+++ lib/services/world_time.dart
@@ -0,0 +1,34 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+import 'package:intl/intl.dart';
+
+class WorldTime {
+  String location; //Location for UI
+  String time; // Time at location
+  String flag; // URL for location flag
+  String url; //location api endpoint
+  bool isDayTime; //true or false if day or not
+
+  WorldTime({this.location, this.flag, this.url});
+
+  Future<void> getTime() async {
+    try{
+      Response response = await get('http://worldtimeapi.org/api/timezone/$url');
+      Map data = jsonDecode(response.body);
+      //get properties from data
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1,3);
+      //Create a datetime object
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
+      //set time property
+      isDayTime = now.hour > 6 && now.hour < 20 ? true : false;
+      time = DateFormat.jm().format(now);
+    }
+    catch (e){
+      print('Caught error $e');
+      time = 'could not get time data';
+    }
+  }
+}
+
