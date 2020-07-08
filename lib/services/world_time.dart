929f7e53e8dbd2def534bdf21485c8311fdc8d64
Vaibhav Muchandi <vaibhavmm@hotmail.com>
2020-07-08 20:14:18 +0530

    added locations with flags and working list

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -22,7 +22,7 @@ class WorldTime {
       DateTime now = DateTime.parse(datetime);
       now = now.add(Duration(hours: int.parse(offset)));
       //set time property
-      isDayTime = now.hour > 6 && now.hour < 20 ? true : false;
+      isDayTime = now.hour > 6 && now.hour < 18 ? true : false;
       time = DateFormat.jm().format(now);
     }
     catch (e){
